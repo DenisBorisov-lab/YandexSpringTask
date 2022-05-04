82c35349a9a8ef17247771a2853923f40c10ab18
DenisBorisov-lab <mai.borisov.denis@gmail.com>
2022-05-04 21:53:27 +0300

    добавил начальное  логирование

--- src/main/java/com/example/yandex/services/DataService.java
+++ src/main/java/com/example/yandex/services/DataService.java
@@ -6,6 +6,8 @@ import com.example.yandex.models.dto.QuizDto;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,7 @@ public class DataService {
     @Autowired
     private ObjectMapper mapper;
 
+    private static final Logger logger = LoggerFactory.getLogger(DataService.class);
     private List<Quiz> data;
 
 
@@ -28,6 +31,7 @@ public class DataService {
         readValue();
         for (QuizDto quizDto : quizDtos) {
             data.add(new Quiz(UUID.randomUUID(), quizDto.getName(), quizDto.getQuestionDtos(), quizDto.getStart(), quizDto.getEnd()));
+            logger.info("Администратор добавил квиз с названием " + quizDto.getName());
         }
         writeData();
     }
@@ -40,14 +44,19 @@ public class DataService {
                 quiz.setName(application.getName());
                 quiz.setQuestions(application.getQuestions());
                 isExist = true;
+
                 break;
             }
         }
 
         if (isExist) {
             writeData();
+            logger.info("Админимтратор изменил квиз с id: " + application.getId());
             return true;
         } else {
+            logger.warn("Админимтратор попытался изменить квиз с id: "
+                    + application.getId()
+                    + " но такого квиза не было найдено!");
             return false;
         }
 
@@ -66,28 +75,34 @@ public class DataService {
 
         if (isExist) {
             writeData();
-            return isExist;
+            logger.info("Админимтратор удалил квиз с id: " + id);
+            return true;
         } else {
+            logger.warn("Администратор поппытался удалить квиз с id: "
+                    + id
+                    + " Но такого квиза не было найдено!");
             return false;
         }
     }
 
     @SneakyThrows
-    public void writeData() {
+    private void writeData() {
         try {
             mapper.writeValue(new File("./src/main/resources/data.json"), data);
         } catch (Exception ex) {
-            System.out.println("не получилось записать в файл");
+            logger.error("Не получилось записать изменения в хранилище");
         }
 
     }
 
     @SneakyThrows
-    public void readValue() {
+    private void readValue() {
         try {
+            logger.debug("Данные из хранилища были успешно прочиатаны");
             data = mapper.readValue(new File("./src/main/resources/data.json"), new TypeReference<List<Quiz>>() {
             });
         } catch (Exception ex) {
+            logger.warn("Не получилось получит данные из хранилища");
             data = new ArrayList<>();
         }
 
